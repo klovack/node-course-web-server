c9ca0583fd110f05c592f1e80783997713511db8
Muhammad Rizki Fikriansyah <rizki.fikriansyah@hotmail.com>
2017-12-26 12:51:02 +0100

    setup start script and ready for heroku

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 const app = express();
 
 hbs.registerPartials(path.join(__dirname, 'views/partials'));
@@ -21,9 +22,11 @@ app.use((req, res, next) => {
   });
   next();
 });
-app.use((req, res, next) => {
-  res.render('maintenance.hbs');
-});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 // REGISTERHELPER
@@ -57,6 +60,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log('Server is up on port ' + port);
 });
